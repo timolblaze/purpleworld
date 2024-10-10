ea6013525c79792d9ebc21be42c5a9e55e68af64
Prospertech-github <ikechukwuprosper8@gmail.com>
2024-10-10 22:09:57 +0100

    FEAT: Cleaned up template and building Login Page

---
+++ src/components/FormComponent.jsx
@@ -0,0 +1,14 @@
+import styles from './FormComponent.module.css'
+
+export default function FormComponent() {
+  return (
+    <div className={styles.container}>
+        <form action="">
+
+        </form>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import DarkBG from '../components/DarkBG'
+import FormComponent from '../components/FormComponent'
+
+export default function Home() {
+  return (
+    <div>
+        <Navbar />
+        <DarkBG text='My Account'> 
+            <p>Home  /  My Account</p>
+        </DarkBG>
+        <FormComponent />
+    </div>
+  )
+}
