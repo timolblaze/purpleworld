1215c4c5b780a2b64b74b9eb3a33aa5c297876d0
Prospertech-github <ikechukwuprosper8@gmail.com>
2024-10-17 00:27:00 +0100

    FEAT: Completed grid section on Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import styles from "./Homepage.module.css";
+import BabyMeal from "../assets/nutribom.png";
+import B2school from "../assets/back2school.png";
+import Nivea from "../assets/nivea.png";
+import Utensils from "../assets/utensils.png";
 
 export default function Homepage() {
   return (
     <>
-        <Navbar />
+      <Navbar />
+      <section className={styles.hero}>
+        <div className={styles.gridSection}>
+          <div className={styles.productCard}>
+            <h3>Baby Cereal</h3>
+            <p> Drinks </p>
+            <a href="3">Shop Now!</a>
+          </div>
 
-        <Footer />
+          <div>
+            <div className={`${styles.productCard} ${styles.small}`}>
+              <h3>Baby Cereal</h3>
+              <p> Drinks </p>
+              <a href="3">Shop Now!</a>
+            </div>
+            <div className={`${styles.productCard} ${styles.small}`}>
+              <h3>Baby Cereal</h3>
+              <p> Drinks </p>
+              <a href="3">Shop Now!</a>
+            </div>
+          </div>
+
+          <div className={`${styles.productCard} ${styles.long}`}>
+            <h3>Baby Cereal</h3>
+            <p> Drinks </p>
+            <a href="3">Shop Now!</a>
+          </div>
+        </div>
+      </section>
+
+      {/* <div></div>
+            <div></div>
+            <div></div>
+        </div> */}
+      <Footer />
     </>
-  )
+  );
 }
