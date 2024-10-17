992d8c567ec0ab637fd4fae1bc723c4babab4205
Prospertech-github <ikechukwuprosper8@gmail.com>
2024-10-18 00:03:01 +0100

    FEAT: Created the Product Card component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,6 +6,13 @@ import BabyMeal from "../assets/nutribom.png";
 import B2school from "../assets/back2school.png";
 import Nivea from "../assets/nivea.png";
 import Utensils from "../assets/utensils.png";
+import Ceralac from "../assets/ceralac.png";
+import Coke from "../assets/drink.png";
+import Diaper from "../assets/smileBaby.png";
+import Coffee from "../assets/coffee.png";
+import SectionTopHeader from "../components/SectionTopHeader";
+import ProductCard from "../components/ProductCard";
+import Kellogs from '../assets/kellogs.png'
 
 export default function Homepage() {
   return (
@@ -41,34 +48,37 @@ export default function Homepage() {
       </section>
 
       <section className={styles.hotSection}>
-        <div className={styles.topHeader}>
-          <h3>New Arrivals</h3>
-          <p>Groceries</p>
+        <SectionTopHeader heading="New Arrivals">
+          <p className={styles.active}>Groceries</p>
           <p>Drinks</p>
           <p>Baby Food </p>
           <p>Skin care</p>
           <p>Home & Kitchen</p>
-        </div>
+        </SectionTopHeader>
 
         <div className={styles.bannerSection}>
           <div>
             <div>
-              <img src="" alt="" />
+              <img src={Ceralac} alt="Ceralac" />
             </div>
             <div>
-              <img src="" alt="" />
+              <img src={Diaper} alt="Diaper wears" />
             </div>
           </div>
           <div>
             <div>
-              <img src="" alt="" />
+              <img src={Coke} alt="Coca Cola" />
             </div>
             <div>
-              <img src="" alt="" />
+              <img src={Coffee} alt="Coffee drink" />
             </div>
           </div>
         </div>
       </section>
+
+      <ProductCard productName='Kellogs Cereal' price='$5.84' category='cereals' imgUrl={Kellogs} alt='Hot'/>
+      <ProductCard productName='Kellogs Cereal' price='$5.84' category='cereals' imgUrl={Kellogs} alt='Hot'/>
+      <ProductCard productName='Kellogs Cereal' price='$5.84' category='cereals' imgUrl={Kellogs} />
       <Footer />
     </>
   );
