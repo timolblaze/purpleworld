ad0891ae41ab6b8d1f45feea7eadfa9aecaed948
Esimehro <loisomodibo@gmail.com>
2024-10-22 21:17:02 +0100

    shopping cart pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter,Routes, Route } from "react-router-dom"
 import Homepage from "./pages/HomePage/Homepage"
 import Login from "./pages/Login"
 import ProductDetails from "./pages/ProductDetails/ProductDetails"
+import ShoppingCart from "./pages/ShoppingCart/ShoppingCart"
 
 
 
@@ -14,6 +15,7 @@ function App() {
         <Route index element={<Homepage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/product" element={<ProductDetails />}/>
+        <Route path="/cart" element={<ShoppingCart/>}/>
       </Routes>
     </BrowserRouter>
     </>
